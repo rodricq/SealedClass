fa7238a0a812fd6154f36e01888a375336c1d142
beverley-dick <beverleydick11@gmail.com>
2023-06-21 12:03:01 +0300

    Add project files.

---
+++ SealedClass/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SealedClass
+{
+    class Animal
+    {
+        public virtual void makeSound()
+        {
+            Console.WriteLine("Animal Sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        //sealed method
+        sealed public override void makeSound()
+        {
+            Console.WriteLine("Dog Sound");
+        }
+    }
+
+    class Puppy : Dog
+    {
+        // trying to overide sealed method
+        public  void makemySound()
+        {
+            Console.WriteLine("Puppy Sound");
+        }
+    }
+
+    class Program
+    {
+        static void Main (string [] args)
+        {
+            //create an object of Puppy class
+            Puppy puppy = new Puppy();
+            puppy.makemySound();
+
+            Dog dog = new Dog();
+            dog.makeSound();
+
+            Animal animal = new Animal();
+            animal.makeSound();
+
+            Console.ReadLine();
+        }
+    }
+}
